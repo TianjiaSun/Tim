5e03d05648bc0f8b6913c58ec96b00763cce28c4
TianjiaSun <thomas.sun@outlook.com>
2015-04-24 16:31:33 -0700

    add link

--- app/js/controller.js
+++ app/js/controller.js
@@ -30,6 +30,16 @@ var store = angular.module('store',['ngRoute','webcam'])
     }
   });
 
+  $scope.addLink = function() {
+    var payload = {
+      op: 'addLink',
+      originAppId: 'Customers'
+    };
+    var targetUrl = (window.location != window.parent.location) ? document.referrer: document.location;
+    parent.postMessage(payload, targetUrl);
+    // console.log(payload);
+  };
+
   // to avoid flashing during page loading
   $scope.init = function () {
     $("#list_container").fadeIn(1000);
